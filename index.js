012c3be47a82007faab512d9cc046d020d696202
chochstrasser <chase.hochstrasser@gmail.com>
2019-02-04 23:18:09 -0700

    factorial page init

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-// Import the library
-const server = require('server');
+const express = require('express');
+const app = express();
 
-// Launch the server to always answer "Hello world"
-server(ctx => 'Hello world, Javascript Algorithms!');
+const factorial = (n) => {
+  console.log('Start: factorial');
+
+  console.log('Result: ' + n);
+  return n;
+};
+
+app.set('view engine', 'html');
+
+// Algorithms
+app.get('/algorithm/:name', (req, res, next) => {
+        var result;
+
+        // determine how to parse the data
+        switch (req.params.name) {
+          case 'factorial':
+            result = factorial(req.query.n);
+            break;
+          default: break;
+        }
+
+        // return result
+        res.json(result);
+});
+
+// HTML files
+app.get('/', (req, res) => {
+  res.redirect('index.html');
+});
+app.get('/:name', (req, res, next) => {
+
+      // get page name
+      const fileName = req.params.name;
+
+      const options = {
+        root: __dirname + '/public/',
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+      };
+
+      // display page
+      res.sendFile(fileName, options, (err) => {
+        if (err) {
+          next(err);
+        } else {
+          console.log('Sent:', fileName);
+        }
+      });
+});
+
+// listen
+app.listen('3000');
